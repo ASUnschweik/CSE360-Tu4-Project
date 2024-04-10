bf03b3341f4f8cda553cf72f441d8aca36ed3c8c
ajrodr54 <166559498+ajrodr54@users.noreply.github.com>
2024-04-10 12:34:41 -0700

    Added Return button for messages

--- GroupProjectCurrent/src/ProjectThree/MessagesScreen.java
+++ GroupProjectCurrent/src/ProjectThree/MessagesScreen.java
@@ -8,9 +8,19 @@ import javafx.geometry.*;
 import ProjectThree.User.UserType;
 import javafx.event.*;
 
-public class MessagesScreen {
+public class MessagesScreen extends GUIScene{
 	public static Node getView() {
 		VBox root = new VBox();
+		
+		Button returnButton = new Button("Return");
+		returnButton.setOnAction(new EventHandler<ActionEvent>() {
+		public void handle(ActionEvent e) {
+			navigateTo(PatientHomepage.getView());
+		}
+		});
+		
+		root.getChildren().add(returnButton);
+		
 		return root;
 	}
 }
