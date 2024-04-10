056531cdcffaa7438063ce9612291e1e32d0bba8
ajrodr54 <166559498+ajrodr54@users.noreply.github.com>
2024-04-10 12:39:06 -0700

    Aded two new MessagesScreens for nurse and doctor

---
+++ GroupProjectCurrent/src/ProjectThree/MessagesScreenDoctor.java
@@ -0,0 +1,26 @@
+package ProjectThree;
+
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.geometry.*;
+import ProjectThree.User.UserType;
+import javafx.event.*;
+
+public class MessagesScreenDoctor extends GUIScene{
+	public static Node getView() {
+		VBox root = new VBox();
+		
+		Button returnButton = new Button("Return");
+		returnButton.setOnAction(new EventHandler<ActionEvent>() {
+		public void handle(ActionEvent e) {
+			navigateTo(MedicalStaffHomepage.getView());
+		}
+		});
+		
+		root.getChildren().add(returnButton);
+		
+		return root;
+	}
+}
